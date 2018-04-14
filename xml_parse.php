2c41aca51982d15753cff5f14926a014ad261b91
Batandwa Colani <i@batandwa.me>
2018-04-14 22:43:10 +0200

    Refactoring.

--- xml_parse.php
+++ xml_parse.php
@@ -3,18 +3,13 @@ $xmlContents = file_get_contents('uploads/xxe.xml');
 $xml = new SimpleXMLElement($xmlContents, $vulnerable ? LIBXML_NOENT : 0);
 $sql = 'INSERT INTO pages (title, keywords, description) VALUES (?, ?, ?)';
 $statement = $mysqli->prepare($sql);
-$statement->bind_param('sss', $title, $keywords, $descr);
+$statement->bind_param('sss', $title, $keywords, $description);
 
 foreach ($xml->property as $prop) {
-    $descr = substr($prop->description, 0, 490);
+    $description = substr($prop->description, 0, 490);
     $keywords = $prop->keywords;
     $title = $prop->title;
     $statement->execute();
 }
 
 $statement->close();
-
-//$dom = new DOMDocument();
-//$dom->loadXML($xmlContents, LIBXML_NOENT | LIBXML_DTDLOAD);
-//
-//print $dom->textContent;
