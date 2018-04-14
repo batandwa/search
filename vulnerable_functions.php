ba0e8e38f84cca06ce8955a33320a2b87433be87
Batandwa Colani <i@batandwa.me>
2018-04-15 00:28:45 +0200

    Return data instead of printing.

--- vulnerable_functions.php
+++ vulnerable_functions.php
@@ -1,12 +1,16 @@
 <?php
 function performSearch($mysqli) {
     $searchkey = $_GET['search'];
+    $output = '';
+
     if ($result = $mysqli->query("SELECT * FROM pages WHERE title like '%$searchkey%' or keywords like '%$searchkey%' or description like '%$searchkey%' ")) {
-        printf("Select returned %d rows.\n", $result->num_rows);
+        $output .= sprintf("Select returned %d rows.\n", $result->num_rows);
         while ($row = mysqli_fetch_array($result)) {
-            print('<div><strong>' . $row['title'] . '</strong>');
-            print('<p>' . $row['description'] . '</p></div>');
+            $output .= '<div><strong>' . $row['title'] . '</strong>';
+            $output .= '<p>' . $row['description'] . '</p></div>';
         }
         $result->close();
     }
+
+    return $output;
 }
