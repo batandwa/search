018865a22ed1b1b6d64ea34faf002fee6dead7a9
Batandwa Colani <i@batandwa.me>
2018-04-15 11:09:44 +0200

    Removed keyword from query.

--- vulnerable_functions.php
+++ vulnerable_functions.php
@@ -3,7 +3,8 @@ function performSearch($mysqli) {
     $searchkey = $_GET['search'];
     $output = '';
 
-    if ($result = $mysqli->query("SELECT * FROM pages WHERE title like '%$searchkey%' or keywords like '%$searchkey%' or description like '%$searchkey%'  LIMIT 10")) {
+    $result = $mysqli->query("SELECT * FROM pages WHERE title like '%$searchkey%' or description like '%$searchkey%'  LIMIT 10");
+    if ($result !== false) {
         $output .= sprintf("Select returned %d rows.\n", $result->num_rows);
         while ($row = mysqli_fetch_array($result)) {
             $output .= '<div><strong>' . $row['title'] . '</strong>';
