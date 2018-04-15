6e970e750b53a973b4a6f1a966435bd916c14f0a
Batandwa Colani <i@batandwa.me>
2018-04-15 10:23:34 +0200

    Reference database connection to separate file.

--- index.php
+++ index.php
@@ -2,12 +2,9 @@
 ini_set('display_errors', 1);
 ini_set('error_reporting', E_ALL);
 
-$mysqli = new mysqli("localhost", "root", "", "safehack");
-if ($mysqli->connect_errno) {
-    die("Application offline.");
-}
+require_once 'db_connect.php';
 
-$vulnerable = false;
+$vulnerable = true;
 require_once $vulnerable ? 'vulnerable_functions.php' : 'safe_functions.php';
 require_once 'functions.php';
 $mode = isset($_GET['mode']) && !empty($_GET['mode']) ? $_GET['mode'] : 'search';
