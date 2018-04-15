4de675c1e1ad1ffcd3c6fc0008331a97d236785e
Batandwa Colani <i@batandwa.me>
2018-04-15 14:09:11 +0200

    Added protection from injected modes.

--- index.php
+++ index.php
@@ -8,4 +8,8 @@ $vulnerable = true;
 require_once $vulnerable ? 'vulnerable_functions.php' : 'safe_functions.php';
 require_once 'functions.php';
 $mode = isset($_GET['mode']) && !empty($_GET['mode']) ? $_GET['mode'] : 'search';
+
+if(!$vulnerable && !in_array($mode, ['upload', 'search', ])) {
+    die('Bad request. What ya tryna pull huh...!?');
+}
 require_once "$mode.php";
